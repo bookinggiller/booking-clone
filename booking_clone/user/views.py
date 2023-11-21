a266ae374c2eb3127bfdf36dcc1b2da521101528
EmineCaki <97104155+EmineCaki@users.noreply.github.com>
2023-11-21 14:10:07 +0300

    user app için view ve path tanımlandı

--- booking_clone/user/views.py
+++ booking_clone/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import *
+from .models import *
 
 # Create your views here.
+
+
+def user_login(request):
+    form = UyeGiris()
+    return render(request,'login.html',{'form':form})
